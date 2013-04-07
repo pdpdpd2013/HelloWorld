c17ad5e018ef42888c43beb4ce31917e6912c348
liduo <liduo@liduotekiMacBook-Air.local>
2013-04-07 21:33:50 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by liduo on 13-4-7.
+//  Copyright (c) 2013年 liduo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
